1c3f692d773bd83f9fefe6550e0d4686f3d1b40f
Wage Folabessy <mbappepsg336@gmail.com>
2025-09-20 22:19:38 +0700

    update endpoint

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000/',
+        target: 'https://api.ocs.efolabessy.app/',
         changeOrigin: true,
         headers: {
           Accept: 'application/json',
@@ -15,7 +15,7 @@ export default defineConfig({
         }
       },
       '/storage': {
-        target: 'http://127.0.0.1:8000/',
+        target: 'https://api.ocs.efolabessy.app/',
         changeOrigin: true,
         headers: {
           Accept: 'application/json',
